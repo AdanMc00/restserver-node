93014de8579d693ab11a3b19da08794b90ee664f
adanmc01 <dev.adanmc01@gmail.com>
2021-01-07 20:47:04 -0600

    variable de entorno MONGODB

--- server/config/config.js
+++ server/config/config.js
@@ -5,9 +5,11 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 let urlDB;
+
+
 if( process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/coffee';
     } else {
-    urlDB = 'mongodb+srv://adan:Butthead12@sexta-gen.an16o.mongodb.net/test'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB
