28ba528c6f1a312744b5d7dad61f7261ebf46578
adanmc01 <dev.adanmc01@gmail.com>
2021-01-09 21:23:33 -0600

    googleSignIn

--- server/config/config.js
+++ server/config/config.js
@@ -4,6 +4,7 @@ process.env.PORT = process.env.PORT || 3000;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 process.env.SEED = process.env.SEED || 'seed-desarrollo'
+process.env.CLIENT_ID = process.env.CLIENT_ID || '262875674340-vgig3ad4jrmlvke8hj4qerrpgsbfv032.apps.googleusercontent.com';
 
 let urlDB;
 
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bcrypt = require('bcrypt');
-const jwt =  require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
+const {OAuth2Client} = require('google-auth-library')
+const client = new OAuth2Client(process.env.CLIENT_ID)
 const Usuario = require('../models/user')
 const app = express()
 
@@ -15,7 +17,7 @@ app.post('/login', (req, res) => {
                 err
             })
         }
-console.log(usuarioDB)
+        console.log(usuarioDB)
         if (!usuarioDB) {
             return res.status(400).json({
                 ok: false,
@@ -35,7 +37,7 @@ console.log(usuarioDB)
 
         let token = jwt.sign({
             usuario: usuarioDB
-        },process.env.SEED,{expiresIn: 60*60*24*30})
+        }, process.env.SEED, {expiresIn: 60 * 60 * 24 * 30})
         res.json({
             ok: true,
             usuario: usuarioDB,
@@ -44,6 +46,94 @@ console.log(usuarioDB)
     })
 
 })
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+
+}
+
+
+app.post('/google', async (req, res) => {
+
+    let token = req.body.idtoken;
+
+    let googleUser = await verify(token)
+        .catch(e => {
+            return res.status(403).json({
+                ok: false,
+                err: e
+            })
+        })
+    console.log("here")
+    Usuario.findOne({email: googleUser.email}, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+        console.log("here2")
+        if (usuarioDB) {
+
+            if (usuarioDB.google === false) {
+
+                    return res.status(500).json({
+                        ok: false,
+                        err:{
+                            message: 'Debe de usar su autenticación normal'
+                        }
+                    })
+                } else {
+                    let token = jwt.sign({
+                        usuario: usuarioDB
+                    }, process.env.SEED, {expiresIn: 60 * 60 * 24 * 30})
+                    return res.json({
+                        ok: true,
+                        usuario: usuarioDB,
+                        token
+                    })
+                }
+            } else {
+
+                // si el usuario no existe en DB
+                let usuario = new Usuario();
+                usuario.nombre = googleUser.nombre
+                usuario.email = googleUser.email
+                usuario.img = googleUser.img
+                usuario.google = true
+                usuario.password = ':)'
+
+                usuario.save((err, usuarioDB) => {
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            err
+                        })
+                    }
+                    let token = jwt.sign({
+                        usuario: usuarioDB
+                    }, process.env.SEED, {expiresIn: 60 * 60 * 24 * 30})
+                    return res.json({
+                        ok: true,
+                        usuario: usuarioDB,
+                        token
+                    })
+                })
+            }
+        })
+  })
 
 module.exports = app;
 
